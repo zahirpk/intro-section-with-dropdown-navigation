5c40752a1cb18d4b25c1491a7e38853058bf5d9a
zaheer <zaheerabbaass@outlook.com>
2023-02-15 20:48:23 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function closeBtn(){
+
+    document.getElementById("btnClose").style.display="none";
+    document.getElementById("btnOpen").style.display="block";
+    document.getElementById("mobileMenu").style.display="none"
+}
+function openBtn(){
+
+    document.getElementById("btnClose").style.display="block";
+    document.getElementById("btnOpen").style.display="none";
+    document.getElementById("mobileMenu").style.display="block"
+} 
+
+
